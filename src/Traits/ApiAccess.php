525be7f35200c45cb5f691fef092befcfbcd2a49
hudecsamuel <hudecsamuel@gmail.com>
2017-03-21 12:44:51 +0100

    Added ApiAccess trait to help implement HasApiAccess interface

---
+++ src/Traits/ApiAccess.php
@@ -0,0 +1,34 @@
+<?php
+namespace Reglendo\MergadoApiModels\Traits;
+
+use MergadoClient\ApiClient;
+
+/**
+ * Class ApiAccess
+ * @package Reglendo\MergadoApiModels\Traits
+ */
+trait ApiAccess
+{
+    /**
+     * @var ApiClient
+     */
+    protected $apiClient;
+
+    /**
+     * @param ApiClient $apiClient
+     * @return mixed
+     */
+    public function setClient(ApiClient $apiClient)
+    {
+        $this->apiClient = $apiClient;
+    }
+
+    /**
+     * @param $token
+     * @return mixed
+     */
+    public function setToken($token)
+    {
+        $this->apiClient->setToken($token);
+    }
+}
